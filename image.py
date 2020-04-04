19fc229a7a6ff4383355cfa74679519dad3ffafb
Ubuntu <ubuntu@ip-172-31-32-124.ec2.internal>
2020-04-04 03:12:54 +0000

    added unsplash api

--- image.py
+++ image.py
@@ -2,12 +2,31 @@ import requests
 from dining import*
 
 
+#*************************************************************************************************************
+#This is the image api from pixabay, but is limited to image search
+
+# ***********************************************************************************************************
+
 name = getfoodName()
 print(name)
 
-url = "https://pixabay.com/api/?key=15869470-8d1d7a626e13a0e0c0234a63f&q="+ name + "&image_type=photo"
+# url = "https://pixabay.com/api/?key=15869470-8d1d7a626e13a0e0c0234a63f&q="+ name + "&image_type=photo"
+
+# response = requests.get(url)
+# json_body = response.json()
+
+# print(json_body["hits"][0]["webformatURL"])
+
+
+
+# *************************************************************************************************************
+
+#This is the image api from unsplash, it has great search but right now my app is in not approved
+
+url ="https://api.unsplash.com/search/photos?query="+ name + "&client_id=sjauyr_gDNXxASXnt9fH3hCPitColEzzz8DVssM12SI"
 
 response = requests.get(url)
 json_body = response.json()
 
-print(json_body["hits"][0]["webformatURL"])
+print(json_body["results"][0]["urls"]["small"])
+
