7836a98bb8e80cfe43c16964e37e9353e5aa5664
Ubuntu <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-05 23:18:59 +0000

    split html into react components

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 
-export class Content extends React.Component {
-    render() {
-        return (<div>
-		<section className="navbar navbar-default navbar-fixed-top" role="navigation">
+function NavigationBar(props){
+  return(
+  <section className="navbar navbar-default navbar-fixed-top" role="navigation">
 			<div className="container">
 				<div className="navbar-header">
 					<a href="#" className="navbar-brand">BearBites</a>
@@ -16,101 +15,48 @@ export class Content extends React.Component {
 				</div>
 			</div>
 		</section>
-		
-    <div className="w3-main w3-content w3-padding" style={{maxWidth:'1200px;margin-top:100px'}}>
-    <div style={{paddingTop: "40px"}}></div>
-    <h3 className="food-time-header">Breakfast 7am-9am</h3>
-    <div className="w3-row-padding w3-center">
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        <div className="w3-quarter" style={{paddingTop:"5%"}}>
-          <div className="food-card">
-            <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-            <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-            <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
-          </div>
-        </div>
-        
+	)
+}
+function FoodCard(props){
+  return( 
+  <div className="w3-row-padding w3-center">
+    <div className="w3-quarter" style={{paddingTop:"5%"}}>
+      <div className="food-card">
+        {/* TODO: need to replace the image and title with data from the json body */}
+        <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
+        <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
+        <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
+      </div>
     </div>
-    <h3 className="food-time-header">Lunch 11am-2pm</h3>
+  </div>
+  )
+}
+
+const breakfastHeader = <h3 className="food-time-header">Breakfast 7am-9am</h3>
+const lunchHeader = <h3 className="food-time-header">Lunch 11am-2pm</h3>
+const dinnerHeader = <h3 className="food-time-header">Dinner 4:30pm-8pm</h3>
+
+export class Content extends React.Component {
+    render() {
+        return (<div>
+        
+		<NavigationBar />
+		
+		{/*Creates margins for our cards */}
+    <div className="w3-main w3-content w3-padding" style={{maxWidth:'1200px;margin-top:100px'}}>
     
-    <h3 className="food-time-header">Dinner 4:30pm-8pm</h3>
+    {/*Creates padding so title starts under navigation bar*/}
+    <div style={{paddingTop: "40px"}}></div>
     
+    {breakfastHeader}
+    <FoodCard />
+    
+    {lunchHeader}
+    <FoodCard />
+
+    
+    {dinnerHeader}
+    <FoodCard />
         
     </div>
     </div>)
