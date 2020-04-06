b4432ab6fe9124d7659593b62c6afad0369c5217
Ubuntu <ubuntu@ip-172-31-32-124.ec2.internal>
2020-04-06 02:58:27 +0000

    has list of title, calorie, image

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
 import flask
+from dining import *
+
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
+
+passed_list = combined_list()
+title_parsed =passed_list[0]
+calorie_parsed = passed_list[1]
+image_parsed = passed_list[2]
+
+print(image_parsed[0])
+
+    
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
