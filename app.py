1faca9a2d9a317278f6bba68655149c7721ed60b
Ubuntu <ubuntu@ip-172-31-89-77.ec2.internal>
2020-04-07 16:38:54 +0000

    Websocket connection established

--- app.py
+++ app.py
@@ -11,18 +11,20 @@ socketio = flask_socketio.SocketIO(app)
 def hello():
     return flask.render_template('index.html')
 
-json_list = combined_list()
-title_parsed =json_list[0]
+# json_list = combined_list()
+# title_parsed =json_list[0]
 # title_parsed =passed_list[0]
 # calorie_parsed = passed_list[1]
 # image_parsed = passed_list[2]
 
-socketio.emit('json_file', {'parsed_data': title_parsed})
-        
-if __name__ =='__main__':
-    socketio.run(
-        app,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=int(os.getenv('PORT', 8080)),
-        debug=True
-    )
+# socketio.emit('json_file', {'parsed_data': title_parsed})
+@socketio.on('connect') 
+def on_connect():
+    print('Someone connected!') 
+    
+socketio.run(
+    app,
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True
+ )
--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Socket } from './Socket';
 
 function NavigationBar(props){
   return(
