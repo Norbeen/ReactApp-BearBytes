7f923573a30b8d6e535830b8bc69bf5f4d305bd7
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-15 22:33:46 +0000

    created basic layout for food review screen

--- scripts/FoodReview.jsx
+++ scripts/FoodReview.jsx
@@ -50,8 +50,27 @@ export class FoodReview extends React.Component {
         <div className="w3-main w3-content w3-padding" style={{maxWidth:'1200px;margin-top:100px'}}>
           {/*Creates padding so title starts under navigation bar*/}
           <div style={{paddingTop: "40px"}}></div>
-          <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"500px", height:"500px", paddingTop: "5%"}} />
-          <h3 className="food-time-header">Food details and Reviews</h3>
+          <div className="w3-row-padding w3-center">
+            <div className="w3-half">
+              <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"400px", height:"400px", paddingTop: "5%", objectFit: "cover"}} />
+            </div>
+            <div className="w3-half" style={{paddingTop: "150px"}}>
+              <h1 className="food-time-header">Jollof Rice</h1>
+              <h3 className="food-time-header">Servings/Calories: </h3>
+              <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{width:"40%", marginLeft: "auto", marginRight: "auto", display: "block"}}/>
+              <h4 className="food-time-header">(0 reviews)</h4>
+            </div>
+          </div>
+          <h1 className="food-review-header">Reviews</h1>
+          <div style={{background:"#F46311", height:"15px", width:"100%"}}></div>
+          <h4 className="food-review-header">Leave a Review</h4>
+          <div className="w3-row">
+            <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
+            <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
+            <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
+            <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
+            <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
+          </div>
         </div>
     </div>)
     }
