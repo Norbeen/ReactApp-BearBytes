6dc304d6ab6e5d58e16f8706297fbbccdcf9cb9c
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-16 21:29:37 +0000

    adding leave a review section of food details view

--- scripts/FoodReview.jsx
+++ scripts/FoodReview.jsx
@@ -71,6 +71,8 @@ export class FoodReview extends React.Component {
             <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
             <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
           </div>
+          <textarea name="" value = {this.state.user_message} className="review-form" placeholder="Type a review..." onChange = {this.handleChangeMessage}></textarea>
+    			<button disabled= {/*!isEnabled */null} onClick={this.handleSubmit} className="review-button">Submit</button>
         </div>
     </div>)
     }
