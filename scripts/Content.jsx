159a8ecaacef6b60a958929650742526e6d1ced7
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-17 00:59:52 +0000

    added filter bar and food review card

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -24,7 +24,7 @@ function FoodCard(props){
     <div className="w3-quarter" style={{paddingTop:"5%"}}>
       <div className="food-card">
       {/* TODO: need to replace the image and title with data from the json body */}
-      <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
+      <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%", objectFit: "cover"}}/>
       <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
       <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
     </div>
@@ -66,15 +66,6 @@ export class Content extends React.Component {
     
        {breakfastHeader}
         <div className="w3-row-padding w3-center">
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
           <FoodCard />
         </div>
     
--- scripts/FoodReview.jsx
+++ scripts/FoodReview.jsx
@@ -20,14 +20,23 @@ function NavigationBar(props){
 }
 function ReviewCard(props){
   return( 
-  <div className="w3-row-padding w3-center">
-    <div className="w3-quarter" style={{paddingTop:"5%"}}>
-      <div className="food-card">
-        {/* TODO: need to replace the image and title with data from the json body */}
-        <img src="https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-2.jpg" alt="Food Item" style={{width:"70%", paddingTop: "5%"}}/>
-        <h5 style={{color: "#F46311"}}>Jollof Rice</h5>
-        <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{paddingBottom: "5%", width:"60%"}}/>
+  <div className="w3-third" style={{paddingTop:"2%"}}>
+    <div className="food-card">
+      <div className="w3-row" >
+        <div className="w3-col" style={{width:"40%"}} >
+          <img src="https://i.pinimg.com/736x/ab/63/54/ab63547c6ecef3a4d542156532f5266e.jpg" alt="profile picture" style={{height: "80px", width:"80px", borderRadius: "50%", paddingTop: "5%", paddingBottom: "5%", objectFit: "cover", float:"right"}}/>
+        </div>
+        <div className="w3-col" style={{width:"60%"}}>
+          <h5 style={{color: "#F46311"}}>Meorge Gartin</h5>
+          <div className="w3-row" >
+            <h7 style={{color: "#F46311"}}>04/16/2020</h7>
+          </div>
+          <img src="https://i.ibb.co/9hPds0L/2Rating.png" alt="Rating" style={{width:"40%"}}/>
+        </div>
       </div>
+      <div className="review-divider"/>
+      <p style={{color: "#F46311", width: "80%", marginLeft:"auto", marginRight: "auto", paddingTop: "2%"}}>The food was delisous today omg! Here is what it looked like today.spiced well-well. </p>
+      <img src="https://gbc-cdn-public-media.azureedge.net/img82551.768x512.jpg" alt="Food Item" style={{width:"200px",height:"200px", paddingBottom: "5%", objectFit: "cover"}}/>
     </div>
   </div>
   )
@@ -72,7 +81,27 @@ export class FoodReview extends React.Component {
             <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
           </div>
           <textarea name="" value = {this.state.user_message} className="review-form" placeholder="Type a review..." onChange = {this.handleChangeMessage}></textarea>
-    			<button disabled= {/*!isEnabled */null} onClick={this.handleSubmit} className="review-button">Submit</button>
+      		<div className="w3-row-padding">	
+      			<button disabled= {/*!isEnabled */null} onClick={this.handleSubmit} className="review-button">Submit</button>
+    			</div>
+    			<div style={{ paddingTop:"3%"}} />
+          <div class="btn-group btn-group-toggle" data-toggle="buttons" style={{backgroundColor:"#F46311"}}>
+            <label className="btn btn-secondary active segmented-control">
+              <input type="radio" name="options" id="Newest" autocomplete="off" checked/> Newest
+            </label>
+            <label className="btn btn-secondary segmented-control">
+              <input type="radio" name="options" id="popular" autocomplete="off"/> Most Popular
+            </label>
+            <label className="btn btn-secondary segmented-control">
+              <input type="radio" name="options" id="positive" autocomplete="off"/> Most Positive
+            </label>
+            <label className="btn btn-secondary segmented-control">
+              <input type="radio" name="options" id="critical" autocomplete="off"/> Most Critical
+            </label>
+          </div>
+          <div className="w3-row-padding w3-center">
+            <ReviewCard />
+          </div>
         </div>
     </div>)
     }
