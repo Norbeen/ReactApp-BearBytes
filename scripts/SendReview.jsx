100ea3e61de6a89f924a044fa24f4e69943d08e4
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-18 22:03:43 +0000

    stores users input to variable and updates the ui with every letter the users types

--- scripts/SendReview.jsx
+++ scripts/SendReview.jsx
@@ -6,9 +6,17 @@ export class SendReview extends React.Component {
     constructor(props) {
     super(props);
     this.state = {
-      menu_data: []
+      review: ""
     };
+    
+    this.handleReviewMessage = this.handleReviewMessage.bind(this);
   }
+  
+  handleReviewMessage(event) {
+        this.setState({review: event.target.value});
+        console.log('user_message', event.target.value);
+    }
+    
     render() {
         return (
     <div>
@@ -19,7 +27,7 @@ export class SendReview extends React.Component {
             <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
             <img src="https://i.ibb.co/XJVKQz1/oie-Co-QW0a-GNUEAp.png" alt="Rating" style={{width:"50px", height:"100%"}}/>
         </div>
-        <textarea name="" value = {this.state.user_message} className="review-form" placeholder="Type a review..." onChange = {this.handleChangeMessage}></textarea>
+        <textarea name="" value = {this.state.review} className="review-form" placeholder="Type a review..." onChange = {this.handleReviewMessage}></textarea>
   		<div className="w3-row-padding">	
   			<button disabled= {/*!isEnabled */null} onClick={this.handleSubmit} className="review-button">Submit</button>
 		</div>
