9ec90eb2e291faae2845d730dfee4f83cdf41ac1
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-23 01:46:11 +0000

    pushing to heroku

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def on_disconnect(data):
 
 @socketio.on('connect') 
 def on_connected():
+    print("somebody connected")
     breakfast_data = models.menuItem.query.filter_by(Utypes='breakfast').all()
     lunch_data = models.menuItem.query.filter_by(Utypes='lunch').all()
     dinner_data = models.menuItem.query.filter_by(Utypes='dinner').all()
