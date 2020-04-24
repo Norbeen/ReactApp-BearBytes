1fa9f2c4a7d8007f369288a581486c473e29dc98
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-04-24 22:23:20 +0000

    double clicking a star resets it

--- scripts/SendReview.jsx
+++ scripts/SendReview.jsx
@@ -14,16 +14,6 @@ function Stars(props){
     	});
     }
     
-    const updateRate = (rate) =>{
-        if (rate == 1){
-            updateRating(0)
-            sendRating(0)
-        }else{
-            updateRating(rate)
-            sendRating(rate)
-        }
-    }
-    
     Socket.on('send review',(data) => {
       console.log('reset')
       updateRating(0)
@@ -31,12 +21,12 @@ function Stars(props){
     
     if (0<= rating <=5){
         for (var i = 0; i < rating; i++) {
-            let current_count = count
             stars.push(
                <input key={count} src="https://i.ibb.co/VHTxrsd/oie-Ic-LCQTSqb-Kj6.png" 
                alt="Rating" 
                onClick={() =>{
-                    updateRate(current_count+1)
+                    updateRating(0)
+                    sendRating(0)
                 }}
                style={{width:"50px", height:"100%"}}
                type="image"
