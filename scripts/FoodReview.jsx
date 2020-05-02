e7a8462ec572c399e76ef5e8d417f07c001d806d
gMakunde <ubuntu@ip-172-31-85-74.ec2.internal>
2020-05-02 03:20:01 +0000

    added other fields to food review

--- scripts/FoodReview.jsx
+++ scripts/FoodReview.jsx
@@ -93,7 +93,9 @@ export class FoodReview extends React.Component {
   }
   
     render() {
-      let food = this.state.menu_data
+      let title = this.state.menu_data.title
+      let calories = this.state.menu_data.calories
+      let rating = this.state.menu_data.rating
         return (
     <div>
 		  <NavigationBar signIn={this.state.signIn} />
@@ -101,7 +103,7 @@ export class FoodReview extends React.Component {
       <div className="w3-main w3-content w3-padding" style={{maxWidth:'1200px;margin-top:100px'}}>
       {/*Creates padding so title starts under navigation bar*/}
       <div style={{paddingTop: "40px"}}></div>
-      <FoodInformation title={food.title} nutri={"1/2 cup(101.6 cal)"} rating={5} reviewCount={1}  />
+      <FoodInformation title={title} nutri={calories} rating={rating} reviewCount={1}  />
       {reviewHeader}
       <SendReview />
   		<div style={{ paddingTop:"3%"}} />
